8d5c4d9f5f5ef3d4e7c58357f31cd9f9c1c4222e
Chris Gonzalez <christogonz92@gmail.com>
2024-07-27 15:01:53 +0200

    Initial Commit

---
+++ AppStorage/AppStorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStorageApp.swift
+//  AppStorage
+//
+//  Created by Christopher Gonzalez on 2024-07-27.
+//
+
+import SwiftUI
+
+@main
+struct AppStorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
